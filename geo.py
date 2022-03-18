fda5e7dde53947742c3da7e578f704bcaaf1c4a2
sovun1a <burindima324@gmail.com>
2022-03-18 19:12:30 +0300

    Alice Yandex

--- geo.py
+++ geo.py
@@ -8,6 +8,7 @@ def get_coordinates(city_name):
         url = "https://geocode-maps.yandex.ru/1.x/"
         # параметры запроса
         params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
             # город, координаты которого мы ищем
             'geocode': city_name,
             # формат ответа от сервера, в данном случае JSON
@@ -17,15 +18,14 @@ def get_coordinates(city_name):
         response = requests.get(url, params)
         # получаем JSON ответа
         json = response.json()
-        # получаем координаты города (там написаны долгота(longitude),
-        # широта(latitude) через пробел).
-        # Посмотреть подробное описание JSON-ответа можно
-        # в документации по адресу
-        # https://tech.yandex.ru/maps/geocoder/
+        # получаем координаты города
+        # (там написаны долгота(longitude), широта(latitude) через пробел)
+        # посмотреть подробное описание JSON-ответа можно
+        # в документации по адресу https://tech.yandex.ru/maps/geocoder/
         coordinates_str = json['response']['GeoObjectCollection'][
             'featureMember'][0]['GeoObject']['Point']['pos']
-        # Превращаем string в список, так как точка -
-        # это пара двух чисел - координат
+        # Превращаем string в список, так как
+        # точка - это пара двух чисел - координат
         long, lat = map(float, coordinates_str.split())
         # Вернем ответ
         return long, lat
@@ -37,14 +37,15 @@ def get_country(city_name):
     try:
         url = "https://geocode-maps.yandex.ru/1.x/"
         params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
             'geocode': city_name,
             'format': 'json'
         }
         data = requests.get(url, params).json()
         # все отличие тут, мы получаем имя страны
-        return data['response']['GeoObjectCollection']['featureMember'][0][
-            'GeoObject']['metaDataProperty']['GeocoderMetaData'][
-            'AddressDetails']['Country']['CountryName']
+        return data['response']['GeoObjectCollection'][
+            'featureMember'][0]['GeoObject']['metaDataProperty'][
+            'GeocoderMetaData']['AddressDetails']['Country']['CountryName']
     except Exception as e:
         return e
 
